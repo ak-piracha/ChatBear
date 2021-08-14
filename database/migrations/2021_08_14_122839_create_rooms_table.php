aaed49dad0c9b3509915afc1a981b93d6902cdbd
Abdul Kareem <abdulkareem.w@outlook.com>
2021-08-14 19:32:24 +0500

    Database migrations, seeder, login and signup, dashboard added

---
+++ database/migrations/2021_08_14_122839_create_rooms_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRoomsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('type_id',false,true);
+            $table->timestamps();
+
+            $table->foreign('type_id')->references('id')->on('room_types');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rooms');
+    }
+}
---
+++ resources/views/components/application-logo.blade.php
@@ -0,0 +1,1 @@
+     <h1>ChatBear</h1>
