10503c4a00be104b7da0c1ab465da87d457c062b
Abdul <abdulkareem.w@outlook.com>
2021-08-17 17:45:36 +0500

    CB-0005: adding a new room and pagination on chatlists

--- app/Http/Livewire/ChatLists.php
+++ app/Http/Livewire/ChatLists.php
@@ -2,22 +2,44 @@
 
 namespace App\Http\Livewire;
 
+use Carbon\Carbon;
 use App\Models\Room;
-use Hamcrest\Core\IsNot;
 use Livewire\Component;
+use App\Models\RoomType;
+use Hamcrest\Core\IsNot;
+use Livewire\WithPagination;    
+use Illuminate\Foundation\Auth\User;
+use Illuminate\Support\Facades\Auth;
 
 class ChatLists extends Component
 {
-    
-    public $rooms;
+    use WithPagination;
+    public $room_name;
 
-    public function mount(Room $room)
+    public function mount()
     {
-        $this->rooms = $room->roomsList();
+        //$this->rooms = $room->roomsList();
     }
 
     public function render()
     {
-        return view('livewire.chat-lists');
+        return view('livewire.chat-lists', [
+            'rooms' => Room::where('user_b_id', '=', Auth::user()->id)
+            ->orWhere('user_a_id', '=', Auth::user()->id)->paginate(6)
+        ]);
+    }
+
+    public function addRoom()
+    {
+        Room::create([
+            'name' => $this->room_name,
+            'user_a_id' => Auth::user()->id,
+            'user_b_id' => User::find(4)->id,
+            'type_id' => RoomType::find(1)->id,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+        
+        $this->reset('room_name'); 
     }
 }
--- resources/views/livewire/chat-lists.blade.php
+++ resources/views/livewire/chat-lists.blade.php
@@ -3,7 +3,11 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
- 
+                    <div class="p-6 bg-white border-b border-gray-200" style="background-color:rgb(197, 197, 197)">
+                        <form wire:submit.prevent="addRoom">
+                        <input wire:model.defer="room_name" type="text" id="room_id" name="room" placeholder="Enter your room name here..."  style="width:100%; background-color:white">
+                    </form> 
+                        </div>
                         @forelse( $rooms as $room)
                         <div class="p-6 bg-white border-b border-gray-200">
                             <a href="/chatroom/{{ $room->id}}">{{ $room->name}} </a> 
@@ -12,7 +16,9 @@
                         @empty
                         No Conversations
                         @endforelse
-                       
+                        <div class="p-6 bg-white border-b border-gray-200">
+                            {{ $rooms->links() }}
+                            </div>
                 </div></div></div></div>
 
 </div>
